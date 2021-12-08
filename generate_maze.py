c302a33ea88aa21cb6b37e87e0770e8e362c1ebe
Saurabh Totey <SaurabhTotey@gmail.com>
2021-12-08 15:11:54 -0700

    Started figuring out structure of maze

--- generate_maze.py
+++ generate_maze.py
@@ -1,28 +1,55 @@
 import sys
+from itertools import chain
 
 class MazeCell:
+
 	def __init__(self):
 		self.visited: bool = False
 		self.is_walkable: bool = False
+
 	def __repr__(self):
-		str(int(not self.is_walkable))
+		return str(int(not self.is_walkable))
 
 class Maze:
+
 	def __init__(self, width: int, height: int):
 		self.width: int = width
 		self.height: int = height
-		self.start_location: tuple[int, int] = (0, 0)
+		self.start_location: tuple[int, int] = (1, 1)
 		self.cells: list[list[MazeCell]] = [[MazeCell() for _ in range(width)] for _ in range(height)]
-	def get_locations_adjacent_to(self, location: tuple[int, int]) -> list[tuple[int, int]]:
+		self.traversable_locations: set[tuple[int, int]] = set([self.start_location])
+		new_traversable_locations = self.get_traversable_locations_adjacent_to(self.start_location)
+		while len(new_traversable_locations) > 0:
+			traversable_locations_to_get_adjacencies_of = []
+			for new_traversable_location in new_traversable_locations:
+				if new_traversable_location in self.traversable_locations:
+					continue
+				self.traversable_locations.add(new_traversable_location)
+				traversable_locations_to_get_adjacencies_of.append(new_traversable_location)
+			new_traversable_locations = list(chain.from_iterable([self.get_traversable_locations_adjacent_to(location) for location in traversable_locations_to_get_adjacencies_of]))
+
+	def __repr__(self):
+		return "\n".join(["".join([repr(cell) for cell in row]) for row in self.cells])
+
+	def is_location_valid(self, location: tuple[int, int]) -> bool:
+		return 0 <= location[0] < self.width and 0 <= location[1] < self.height
+
+	def get_traversable_locations_adjacent_to(self, location: tuple[int, int]) -> list[tuple[int, int]]:
 		adjacent_locations = [
-			(location[0], location[1] - 1)
-			(location[0], location[1] + 1)
-			(location[0] - 1, location[1])
-			(location[0] + 1, location[1])
+			(location[0], location[1] - 2),
+			(location[0], location[1] + 2),
+			(location[0] - 2, location[1]),
+			(location[0] + 2, location[1])
 		]
-		return [adjacent_location for adjacent_location in adjacent_locations if 0 <= adjacent_location[0] < self.width and 0 <= adjacent_location[1] < self.height]
+		return [adjacent_location for adjacent_location in adjacent_locations if self.is_location_valid(adjacent_location)]
+
 	def get_cell_at(self, location: tuple[int, int]) -> MazeCell:
 		return self.cells[location[1]][location[0]]
+
+def generate_hunt_and_kill_maze(width, height) -> Maze:
+	maze = Maze(width, height)
+	# TODO:
+	return maze
 
 if __name__ == "__main__":
 	width, height = 50, 50
@@ -34,3 +61,4 @@ if __name__ == "__main__":
 	if len(sys.argv) > 3:
 		save_location = sys.argv[3]
 	print(f"Generating a maze of width {width} and height {height} to {save_location}.")
+	maze = generate_hunt_and_kill_maze(width, height)
