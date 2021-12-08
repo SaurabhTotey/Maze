165eedaaa5ec98f56d4dc67fe5a30775196baca5
Saurabh Totey <SaurabhTotey@gmail.com>
2021-12-08 14:29:50 -0700

    Untested maze class for maze generation

--- generate_maze.py
+++ generate_maze.py
@@ -1,5 +1,29 @@
 import sys
 
+class MazeCell:
+	def __init__(self):
+		self.visited: bool = False
+		self.is_walkable: bool = False
+	def __repr__(self):
+		str(int(not self.is_walkable))
+
+class Maze:
+	def __init__(self, width: int, height: int):
+		self.width: int = width
+		self.height: int = height
+		self.start_location: tuple[int, int] = (0, 0)
+		self.cells: list[list[MazeCell]] = [[MazeCell() for _ in range(width)] for _ in range(height)]
+	def get_locations_adjacent_to(self, location: tuple[int, int]) -> list[tuple[int, int]]:
+		adjacent_locations = [
+			(location[0], location[1] - 1)
+			(location[0], location[1] + 1)
+			(location[0] - 1, location[1])
+			(location[0] + 1, location[1])
+		]
+		return [adjacent_location for adjacent_location in adjacent_locations if 0 <= adjacent_location[0] < self.width and 0 <= adjacent_location[1] < self.height]
+	def get_cell_at(self, location: tuple[int, int]) -> MazeCell:
+		return self.cells[location[1]][location[0]]
+
 if __name__ == "__main__":
 	width, height = 50, 50
 	save_location = "map.txt"
